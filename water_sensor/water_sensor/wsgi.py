221bae4059cd16939a1978a502a9a5d6da354f8f
Egesa Wenslous Otema <wayneotemahegesa@gmail.com>
2022-10-08 20:26:19 +0300

    changes wsgi.py

--- water_sensor/water_sensor/wsgi.py
+++ water_sensor/water_sensor/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'water_sensor.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
